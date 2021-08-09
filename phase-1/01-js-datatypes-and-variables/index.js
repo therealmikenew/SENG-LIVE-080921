89b153f5a9f14fbe7f9d65dd51e0ac1b4bb3b513
Aysan Isayo <aisayo@icloud.com>
2021-08-09 13:18:19 -0700

    lecture code

---
+++ phase-1/01-js-datatypes-and-variables/index.js
@@ -0,0 +1,82 @@
+// String 
+// "Hi, I'm Aysan!"
+// `Hi, I'm Aysan!`
+// 'Hi, I\'m Aysan!'
+
+// Numbers 
+// 5 // integer
+// // 1.8 // floats 
+// // -5 
+// // NaN
+
+// // Booleans 
+// // returns truthy or falsey value 
+
+// // falsey 
+// 0 
+// ""
+// undefined 
+// null 
+// false
+// NaN
+
+// // everything else is going to return true 
+
+// // primitive types vs non primitive types 
+// // primitive types can store a single value
+// // non primitive can store a collection of data
+
+// // operators 
+// let likes = 0
+// // likes = likes + 1
+// // likes += 1
+// // likes++
+// // console.log(++likes)
+// // ++likes
+// // console.log(1 - 1)
+// // console.log(3 === '3') 
+// // console.log("" == 0)
+
+// // are undefined and null the same thing?
+// console.log(typeof null)
+// console.log(typeof undefined)
+
+
+// Variables 
+// placeholders
+// containers to hold data
+// used to store data and values
+// makes code easier to read
+// useful to access data later in the code
+
+// what keywords are used in defining variables
+// let
+// var
+// const 
+
+// var has some side effects 
+
+// let dog; 
+// dog = "pooch"
+
+// let likes = 0
+// ++likes
+// console.log(likes)
+
+// const likes = 0
+// ++likes
+// console.log(likes)
+
+// variable naming best practices
+// make it clear what the value is returning 
+// naming things plural vs singular
+// should never start with numbers
+
+// pokemon = "Pikachu"
+// console.log(pokemon)
+
+likes = 0
+console.log(likes)
+if (true) {
+    let likes = 0
+}
