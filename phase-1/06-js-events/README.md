fda120db077e8c995944f5956b76cca2754e9cd7
Aysan Isayo <aisayo@icloud.com>
2021-08-15 19:16:49 -0700

    lecture starter code

---
+++ phase-1/06-js-events/README.md
@@ -0,0 +1,7 @@
+# JS Events
+
+### Today's Goals:
+
+1. Add an event to like button and increase likes upon event reaction
+2. Process form submission and append values to DOM
+3. Allow user to delete a created Pokemon
---
+++ phase-1/06-js-events/index.js
@@ -0,0 +1,72 @@
+const pokemons = [
+  {
+    id: 1,
+    name: "bulbasaur",
+    img: "https://static.wikia.nocookie.net/kingdom-keymasters-database/images/2/27/001Bulbasaur_XY_anime.png/revision/latest?cb=20160927122032",
+    likes: 4,
+  },
+  {
+    id: 2,
+    name: "ivysaur",
+    img: "https://static.wikia.nocookie.net/kingdom-keymasters-database/images/c/ca/002Ivysaur_XY_anime_2.png/revision/latest?cb=20161017212021",
+    likes: 21,
+  },
+  {
+    id: 3,
+    name: "venusaur",
+    img: "https://images.saymedia-content.com/.image/t_share/MTc2MjYwODQ5NTk2NTcyODYy/pokemon-venusaur-nicknames.png",
+    likes: 7,
+  },
+  {
+    id: 4,
+    name: "charmander",
+    img: "https://pixy.org/download/1207107/",
+    likes: 20,
+  },
+  {
+    id: 5,
+    name: "charmeleon",
+    img: "https://static.wikia.nocookie.net/pokemon-quest-adventures/images/a/a4/005Charmeleon_XY_anime_2.png/revision/latest?cb=20170313041459",
+    likes: 11,
+  },
+];
+
+const pokeContainer = document.getElementById("poke-container");
+const pokeForm = document.getElementById("poke-form");
+
+function renderPokemon(pokemon) {
+  const pokeCard = document.createElement("div");
+  pokeCard.id = `poke-${pokemon.id}`;
+  pokeCard.className = "poke-card";
+
+  const pokeImg = document.createElement("img");
+  pokeImg.src = pokemon.img;
+  pokeImg.alt = `${pokemon.name} image`;
+
+  const pokeName = document.createElement("h3");
+  pokeName.textContent = pokemon.name;
+
+  const pokeLikes = document.createElement("h3");
+  pokeLikes.textContent = "Likes: ";
+
+  const likesNum = document.createElement("h5");
+  likesNum.className = "like-num";
+  likesNum.textContent = pokemon.likes;
+
+  const likeBttn = document.createElement("button");
+  likeBttn.className = "like-bttn";
+  likeBttn.textContent = "♥";
+  
+  const deleteBttn = document.createElement("button");
+  deleteBttn.className = "delete-bttn";
+  deleteBttn.textContent = "Delete";
+
+  pokeCard.append(pokeImg, pokeName, pokeLikes, likesNum, likeBttn, deleteBttn);
+  pokeContainer.appendChild(pokeCard);
+}
+
+function init() {
+  pokemons.forEach(renderPokemon);
+}
+
+init()
