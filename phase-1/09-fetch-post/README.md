b6d5c85b677fc232c09371d3bfbdf0e9a31884cb
Aysan Isayo <aisayo@icloud.com>
2021-08-19 09:33:41 -0700

    student starter code

---
+++ phase-1/09-fetch-post/README.md
@@ -0,0 +1,14 @@
+# Making a POST request with Fetch
+
+### Goals
+
+- Execute post requests
+- Explain the difference between optimistic and pessimistic rendering
+- Explain the difference between each HTTP method
+
+To use the mock-API:
+First install JSON Server: `npm install -g json-server`
+To start the server run `json-server --watch db.json`
+This will produce the following endpoint: `http://localhost:3000/pokemons`
+
+
---
+++ phase-1/09-fetch-post/index.js
@@ -0,0 +1,71 @@
+const pokeContainer = document.getElementById("poke-container");
+const pokeForm = document.getElementById("poke-form");
+
+function getPokemon() {
+  fetch("http://localhost:3000/pokemons")
+    .then((resp) => resp.json())
+    .then((pokemons) => pokemons.forEach(renderPokemon));
+}
+
+function renderPokemon(pokemon) {
+  const pokeCard = document.createElement("div");
+  pokeCard.id = `poke-${pokemon.id}`;
+  pokeCard.className = "poke-card";
+
+  const pokeImg = document.createElement("img");
+  pokeImg.src = pokemon.img;
+  pokeImg.alt = `${pokemon.name} image`;
+
+  const pokeName = document.createElement("h3");
+  pokeName.textContent = pokemon.name;
+
+  const pokeLikes = document.createElement("h3");
+  pokeLikes.textContent = "Likes: ";
+
+  const likesNum = document.createElement("h5");
+  likesNum.className = "like-num";
+  likesNum.textContent = pokemon.likes;
+
+  const likeBttn = document.createElement("button");
+  likeBttn.className = "like-bttn";
+  likeBttn.textContent = "♥";
+  likeBttn.addEventListener("click", () => increaseLike(pokemon, likesNum));
+
+  const deleteBttn = document.createElement("button");
+  deleteBttn.className = "delete-bttn";
+  deleteBttn.textContent = "Delete";
+  deleteBttn.addEventListener("click", () => deletePoke(pokeCard));
+
+  pokeCard.append(pokeImg, pokeName, pokeLikes, likesNum, likeBttn, deleteBttn);
+  pokeContainer.appendChild(pokeCard);
+}
+
+function createPokemon(event) {
+  event.preventDefault();
+  let pokeName = pokeForm.querySelector("#name-input").value;
+  let pokeImg = pokeForm.querySelector("#img-input").value;
+  let pokemon = {
+    id: 7, // needs to change
+    name: pokeName,
+    img: pokeImg,
+    likes: 0,
+  };
+  renderPokemon(pokemon);
+  pokeForm.reset();
+}
+
+function increaseLike(pokemon, likesElement) {
+  ++pokemon.likes;
+  likesElement.textContent = pokemon.likes;
+}
+
+function deletePoke(pokeCard) {
+  pokeCard.remove();
+}
+
+function init() {
+  getPokemon();
+  pokeForm.addEventListener("submit", createPokemon);
+}
+
+init();
